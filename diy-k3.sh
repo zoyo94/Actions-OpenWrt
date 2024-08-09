ed9e616880ea806e32c2566d1b99a6a818dc2704
杨哲 <57941145+zoyo94@users.noreply.github.com>
2024-08-10 00:01:47 +0800

    Update diy-k3.sh

--- diy-k3.sh
+++ diy-k3.sh
@@ -30,6 +30,8 @@ sed -i "245a\                set system.@system[-1].zonename='Asia/Shanghai'" ./
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
+# 只编译K3
+sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' ./target/linux/bcm53xx/image/Makefile
 
 
 # 使用源码自带ShadowSocksR Plus+出国软件(与下方的vssr重复可以注释掉，文件不做修改)
