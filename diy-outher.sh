3b4eb5ca727cb4dd3a1ffb8cf00008ac34706e6a
杨哲 <57941145+zoyo94@users.noreply.github.com>
2025-01-11 23:04:14 +0800

    Update diy-outher.sh

--- diy-outher.sh
+++ diy-outher.sh
@@ -1,8 +1,8 @@
 # 取消掉luci-theme-bootstrap默认
-sed -i 's/+luci-theme-bootstrap//g' ./feeds/luci/collections/luci/Makefile
+#sed -i 's/+luci-theme-bootstrap//g' ./feeds/luci/collections/luci/Makefile
 
 # 修改默认语言主题
 sed -i 's/auto/"zh_cn"/g' feeds/luci/modules/luci-base/root/etc/config/luci
 sed -i '15a\        option zh_cn "普通话(chinese)"' feeds/luci/modules/luci-base/root/etc/config/luci
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+#rm -rf feeds/packages/lang/golang
+#git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
