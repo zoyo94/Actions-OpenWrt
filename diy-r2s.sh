266a95287af328906993c4acd481ab18035eebde
杨哲 <57941145+zoyo94@users.noreply.github.com>
2025-12-23 14:16:06 +0800

    Update diy-r2s.sh

--- diy-r2s.sh
+++ diy-r2s.sh
@@ -10,15 +10,15 @@
 # See /LICENSE for more information.
 #
 # 修改openwrt登陆地址,把下面的192.168.2.2修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/luci/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/files/bin/config_generate
 
 
 # 修改主机名字，把OpenWrt-123修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/LEDE/R2S/g' ./package/base-files/luci/bin/config_generate
+sed -i 's/LEDE/R2S/g' ./package/base-files/files/bin/config_generate
 
 # 设定时区
 sed -i 's/UTC/CST-8/g' ./package/base-files/luci/bin/config_generate
-sed -i "245a\                set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/luci2/bin/config_generate
+sed -i "245a\                set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
