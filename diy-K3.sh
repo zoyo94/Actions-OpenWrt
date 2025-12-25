1b5d32d30e8e72d109dfda1ec039e20979e38853
杨哲 <57941145+zoyo94@users.noreply.github.com>
2025-12-25 15:43:14 +0800

    Modify timezone configuration in diy-K3.sh
    Update timezone settings to Asia/Shanghai and CST-8.

--- diy-K3.sh
+++ diy-K3.sh
@@ -24,9 +24,8 @@ sed -i 's/192.168.1.1/192.168.5.1/g' ./package/base-files/files/bin/config_gener
 sed -i 's/OpenWrt/ZOYO/g' ./package/base-files/files/bin/config_generate
 
 # 设定时区
-sed -i 's/UTC/CST-8/g' ./package/base-files/files/bin/config_generate
-sed -i "245a\                set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
-
+sed -i "s/zonename='UTC'/zonename='Asia\/Shanghai'/" ./package/base-files/files/bin/config_generate
+sed -i "s/timezone='[^']*'/timezone='CST-8'/" ./package/base-files/files/bin/config_generate
 
 # 设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' ./package/lean/default-settings/files/zzz-default-settings
